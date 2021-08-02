f47344506a5d1a07c18fafaac479c3537b468617
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-02 17:57:20 +0530

    [SaiVijayaKumar] Commiting welcome message

---
+++ SOLIDPrinciples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SOLID Desigin Principle");
+        }
+    }
+}
